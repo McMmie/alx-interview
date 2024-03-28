ce964dc48d335673ddf1955c2feb9fb335c9f68a
McMmie <tharausamuel01@gmail.com>
2024-03-28 22:34:41 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,16 +5,15 @@ parses them, and provides statistics.
 """
 
 import sys
-from collections import defaultdict
 
 
 if __name__ == '__main__':
     # Initialize variables
-    file_size, count = 0, 0
+    filesize, count = 0, 0
     codes = {"200", "301", "400", "401", "403", "404", "405", "500"}
     stats = {k: 0 for k in codes}
 
-    def print_stats(stats: Counter, file_size: int) -> None:
+    def print_stats(stats: dict, file_size: int) -> None:
         """
         Prints statistics based on status codes and total file size.
 
@@ -34,14 +33,14 @@ if __name__ == '__main__':
         for line in sys.stdin:
             count += 1
             data = line.split()
-                try:
-                    status_code = data[-2]
-                        if status_code in stats:
-                            stats[status_code] += 1
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
 
-                except BaseException:
-                    pass
-                try:
+            except BaseException:
+                pass
+            try:
                     file_size += int(data[-1])
                 except BaseException:
                     pass
